456959763aabafa5fa86ee1b90bc197fca81fe85
Mrinal Singh <mrinalsingh7000@gmail.com>
2025-07-05 17:50:48 +0530

    basic react-js commit

---
+++ demos/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Greet from "./components/Greet";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <>
+      {/* <div>App</div>
+      <Greet /> */}
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
---
+++ demos/src/components/MainContent.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const MainContent = () => {
+  return (
+    <main>
+      <h2>Main Content</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit
+        magni corporis, rerum maxime officiis animi, deleniti, facere beatae nam
+        a aperiam labore. Aliquam voluptatum neque excepturi quod deserunt dolor
+        iure!
+      </p>
+    </main>
+  );
+};
+
+export default MainContent;
